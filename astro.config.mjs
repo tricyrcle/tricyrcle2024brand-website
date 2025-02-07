756beaa900aef8ec708b43de9000a7ec7dd0acaf
Tom Marble <tmarble@info9.net>
2025-02-07 12:47:15 -0600

    redirects for cantina terms and privacy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/blog/privacy-policy': 'https://cantinavpn.com/privacy/',
+    '/blog/terms-of-use': 'https://cantinavpn.com/terms/'
+  }
+});
